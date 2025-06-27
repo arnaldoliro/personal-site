da996d7e614c5bd075d17fd5b5fedc8790b07888
Arnaldo Liro <dinholiro@gmail.com>
2025-06-27 14:16:40 -0300

    feat: animating buttons and fixing responsiveness bugs

---
+++ src/data/navlinks.ts
@@ -0,0 +1,6 @@
+export const navLinks = [
+  { href: "#home", label: "Início" },
+  { href: "#about", label: "Sobre Mim" },
+  { href: "#projects", label: "Projetos" },
+  { href: "#contact", label: "Contato" },
+];
