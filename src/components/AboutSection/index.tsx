164de85a1d8d370b439a1e78da1527a11fa2e08e
Arnaldo Liro <dinholiro@gmail.com>
2025-07-16 12:00:46 -0300

    feat: structuring the skill card outline

--- src/components/AboutSection/index.tsx
+++ src/components/AboutSection/index.tsx
@@ -1,10 +1,12 @@
+import Card from "../CardSkills"
+
 export default function AboutSection() {
    return(  
     <section id="about" className="py-20 bg-gray-900 text-white">
       <div className="container mx-auto px-4">
         <h2 className="text-3xl text-center font-bold mb-6">Sobre mim</h2>
-        <div className="flex md:flex-row gap-6 mx-auto items-center">
-            <div className="container">
+        <div className="flex gap-6 mx-auto items-center">
+            <div className="">
                 <h1 className="text-xl font-bold mb-4">Minha Jornada</h1>
                 <div className="flex">
                     <div>
@@ -24,22 +26,12 @@ export default function AboutSection() {
                 </div>
                 </div>
             </div>
-            <div>
+            <div className="">
                 <h1 className="text-xl font-bold mb-4">Minhas Habilidades</h1>
-                <div>
-                    {/*Isso vai virar um componente card de habilidades*/}
-                    <ul className="flex">
-                        <li>
-                            <p className="text-lg">
-                                Além de programar, também sou um entusiasta do design de experiências digitais, sempre buscando melhorar a usabilidade e a estética dos projetos.
-                            </p>
-                        </li>
-                        <li>
-                            <p className="text-lg">
-                                Tenho experiência em tecnologias como React, Node.js, e estou sempre aprendendo novas ferramentas e frameworks.
-                            </p>
-                        </li>
-                    </ul>
+                <div className="flex gap-6">
+                    {/* Usar um map para o card de habilidades */}
+                    <Card />
+                    <Card />
                 </div>
             </div>
         </div>
---
+++ src/components/CardSkills/index.tsx
@@ -0,0 +1,8 @@
+export default function Card() {
+    return(
+        <div className="bg-gray-800 p-4 rounded-lg shadow-sm shadow-gray-950 hover:shadow-black hover:shadow-lg hover:scale-105 transition-all duration-300">
+            <h3 className="text-lg font-semibold mb-2">Desenvolvimento Frontend</h3>
+            <p className="text-gray-400">Descrição da habilidade.</p>
+        </div>
+    )
+}
