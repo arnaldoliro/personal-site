6b7b9948be29c6396b559cdf98d5f8e4f61de18b
Arnaldo Liro <dinholiro@gmail.com>
2025-07-23 14:04:27 -0300

    feat: styling components and buttons and animating them

--- src/components/ContactSection/index.tsx
+++ src/components/ContactSection/index.tsx
@@ -1,5 +1,6 @@
 import SectionDescription from "../SectionDescription";
 import SectionTitle from "../SectionTitle";
+import { Github, Linkedin, Mail, MapPin, Phone, Send } from "lucide-react";
 
 export default function ContactSection() {
   return (
@@ -13,7 +14,7 @@ export default function ContactSection() {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mt-10 items-start">
-          <form className="w-full max-w-lg bg-[#1e1e1e] p-6 rounded-2xl shadow-lg mx-auto md:mx-0">
+          <form className="w-full h-full max-w-lg bg-[#1e1e1e] p-6 rounded-2xl shadow-lg mx-auto md:mx-0">
             <h1 className="text-2xl font-semibold text-center mb-6">Envie uma mensagem</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               <div className="flex flex-col">
@@ -36,7 +37,7 @@ export default function ContactSection() {
               </div>
             </div>
 
-            <div className="mt-4">
+            <div className="mt-8">
               <label className="text-sm" htmlFor="message">Mensagem</label>
               <textarea
                 id="message"
@@ -48,21 +49,44 @@ export default function ContactSection() {
 
             <button
               type="submit"
-              className="mt-6 inline-block bg-custom-gradient text-[#171717] font-semibold py-3 px-6 rounded-lg shadow-lg hover:shadow-[#f974165b] transition-all duration-500 w-full"
+              className="mt-14 inline-block bg-custom-gradient text-[#171717] font-semibold py-3 px-6 rounded-lg shadow-lg hover:shadow-[#f974165b] transition-all duration-500 w-full cursor-pointer"
             >
               Enviar Mensagem
             </button>
           </form>
           <div className="space-y-6 w-full max-w-md mx-auto md:mx-0">
             <div className="bg-[#1e1e1e] p-6 rounded-2xl shadow-lg">
-              <h1 className="text-lg font-semibold mb-4">Informações de Contato</h1>
-              <p className="text-sm">Email: seuemail@example.com</p>
-              <p className="text-sm">Telefone: (00) 12345-6789</p>
+              <h1 className="text-xl font-semibold mb-8">Informações de Contato</h1>
+              <div className="flex items-center gap-5 mb-5">
+                <span className="bg-gray-800 rounded-full p-4"><Mail size={20}/></span>
+                <div>
+                  <p className="text-sm text-gray-400">Email</p>
+                  <p>dinholiro@gmail.com</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-5 mb-5">
+                <span className="bg-gray-800 rounded-full p-4"><Phone size={20}/></span>
+               <div>
+                  <p className="text-sm text-gray-400">Telefone</p>
+                  <p>+55 71 98668-8603</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-5 mb-5">
+                <span className="bg-gray-800 rounded-full p-4"><MapPin size={20}/></span>
+                <div>
+                  <p className="text-sm text-gray-400">Localização</p>
+                  <p>Salvador, Brasil</p>
+                </div>
+              </div>
             </div>
             <div className="bg-[#1e1e1e] p-6 rounded-2xl shadow-lg">
-              <h1 className="text-lg font-semibold mb-4">Redes Sociais</h1>
-              <p className="text-sm">Twitter: @seutwitter</p>
-              <p className="text-sm">LinkedIn: seu-linkedin</p>
+              <h1 className="text-xl font-semibold mb-8">Redes Sociais</h1>
+              <div className="flex gap-4 justify-center">
+                <a className="rounded-full bg-[#333333] p-4 hover:bg-gradient-to-br hover:from-orange-500 hover:to-yellow-400 hover:text-gray-900 hover:shadow-lg shadow-[#ff901288] hover:scale-102 transition-all duration-300 cursor-pointer transform hover:-translate-y-1" href="https://github.com/seugithub" target="_blank" rel="noopener noreferrer"><Github size={20}/></a>
+                <a className="rounded-full bg-[#333333] p-4 hover:bg-gradient-to-br hover:from-orange-500 hover:to-yellow-400 hover:text-gray-900 hover:shadow-lg shadow-[#ff901288] hover:scale-102 transition-all duration-300 cursor-pointer transform hover:-translate-y-1" href="https://linkedin.com/in/seulinkedin" target="_blank" rel="noopener noreferrer"><Linkedin size={20}/></a>
+                <a className="rounded-full bg-[#333333] p-4 hover:bg-gradient-to-br hover:from-orange-500 hover:to-yellow-400 hover:text-gray-900 hover:shadow-lg shadow-[#ff901288] hover:scale-102 transition-all duration-300 cursor-pointer transform hover:-translate-y-1" href="" target="_blank" rel="noopener noreferrer"><Mail size={20}/></a>
+                <a className="rounded-full bg-[#333333] p-4 hover:bg-gradient-to-br hover:from-orange-500 hover:to-yellow-400 hover:text-gray-900 hover:shadow-lg shadow-[#ff901288] hover:scale-102 transition-all duration-300 cursor-pointer transform hover:-translate-y-1" href="" target="_blank" rel="noopener noreferrer"><Send size={20}/></a>
+              </div>
             </div>
           </div>
         </div>
