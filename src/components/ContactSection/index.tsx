7348788f97faff40071465b7f6936c0de5ae878f
Arnaldo Liro <dinholiro@gmail.com>
2025-07-24 10:45:59 -0300

    fix: adjusting spacing, style and animation of components

--- src/components/ContactSection/index.tsx
+++ src/components/ContactSection/index.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { motion } from "framer-motion";
 import SectionDescription from "../SectionDescription";
 import SectionTitle from "../SectionTitle";
 import { Github, Linkedin, Mail, MapPin, Phone, Send } from "lucide-react";
@@ -14,48 +17,60 @@ export default function ContactSection() {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mt-10 items-start">
-          <form className="w-full h-full max-w-lg bg-[#1e1e1e] p-6 rounded-2xl shadow-lg mx-auto md:mx-0">
+          <motion.form 
+           className="w-full h-full max-w-lg bg-[#1e1e1e] p-6 rounded-2xl shadow-lg mx-auto md:mx-0"
+           initial={{ opacity: 0, x: -100 }}
+           whileInView={{ opacity: 1, x: 0 }}
+           transition={{ duration: 0.7 }}
+           viewport={{ once: false, amount: 0.3 }}
+          >
             <h1 className="text-2xl font-semibold text-center mb-6">Envie uma mensagem</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <div className="flex flex-col">
-                <label className="text-sm" htmlFor="name">Nome</label>
+            <div className="grid grid-cols-1 md:grid-cols-1 gap-4">
+              <div className="flex flex-col gap-1">
+                <label className="text-sm font-semibold" htmlFor="name">Nome</label>
                 <input
                   type="text"
                   id="name"
                   placeholder="Seu Nome"
-                  className="p-3 rounded-lg bg-[#1e1e1e] border border-[#333] focus:outline-none focus:ring-2 focus:ring-custom-gradient"
+                  className="p-3 w-full rounded-lg bg-[#1e1e1e] border border-[#333] focus:outline-none focus:border-[1px] focus:border-[#f97416] focus:shadow-sm shadow-[#f97416] transition-all duration-300"
                 />
               </div>
-              <div className="flex flex-col">
-                <label className="text-sm" htmlFor="email">Email</label>
+              <div className="flex flex-col gap-1">
+                <label className="text-sm font-semibold" htmlFor="email">Email</label>
                 <input
                   type="email"
                   id="email"
                   placeholder="Seu Email"
-                  className="p-3 rounded-lg bg-[#1e1e1e] border border-[#333] focus:outline-none focus:ring-2 focus:ring-custom-gradient"
+                  className="p-3 rounded-lg bg-[#1e1e1e] border border-[#333] focus:outline-none focus:border-[1px] focus:border-[#f97416] focus:shadow-sm shadow-[#f97416] transition-all duration-300"
                 />
               </div>
             </div>
 
-            <div className="mt-8">
-              <label className="text-sm" htmlFor="message">Mensagem</label>
+            <div className="mt-4 flex flex-col gap-1">
+              <label className="text-sm font-semibold" htmlFor="message">Mensagem</label>
               <textarea
                 id="message"
                 placeholder="Sua Mensagem"
-                className="w-full mt-2 p-3 rounded-lg bg-[#1e1e1e] border border-[#333] focus:outline-none focus:ring-2 focus:ring-custom-gradient"
+                className="textarea-scrollbar resize-none p-3 rounded-lg bg-[#1e1e1e] border border-[#333] focus:outline-none focus:border-[1px] focus:border-[#f97416] focus:shadow-sm shadow-[#f97416] transition-all duration-300"
                 rows={5}
               />
             </div>
 
             <button
               type="submit"
-              className="mt-14 inline-block bg-custom-gradient text-[#171717] font-semibold py-3 px-6 rounded-lg shadow-lg hover:shadow-[#f974165b] transition-all duration-500 w-full cursor-pointer"
+              className="mt-6 inline-block bg-custom-gradient text-[#171717] font-semibold py-3 px-6 rounded-lg shadow-lg hover:shadow-[#f974165b] transition-all duration-500 w-full cursor-pointer"
             >
               Enviar Mensagem
             </button>
-          </form>
-          <div className="space-y-6 w-full max-w-md mx-auto md:mx-0">
-            <div className="bg-[#1e1e1e] p-6 rounded-2xl shadow-lg">
+          </motion.form>
+          <div className="space-y-11 w-full max-w-md mx-auto md:mx-0">
+            <motion.div 
+             className="bg-[#1e1e1e] p-6 rounded-2xl shadow-lg"
+             initial={{ opacity: 0, x: 100 }}
+             whileInView={{ opacity: 1, x: 0 }}
+             transition={{ duration: 0.7 }}
+             viewport={{ once: false, amount: 0.3 }}
+            >
               <h1 className="text-xl font-semibold mb-8">Informações de Contato</h1>
               <div className="flex items-center gap-5 mb-5">
                 <span className="bg-gray-800 rounded-full p-4"><Mail size={20}/></span>
@@ -78,8 +93,14 @@ export default function ContactSection() {
                   <p>Salvador, Brasil</p>
                 </div>
               </div>
-            </div>
-            <div className="bg-[#1e1e1e] p-6 rounded-2xl shadow-lg">
+            </motion.div>
+            <motion.div 
+             className="bg-[#1e1e1e] p-6 rounded-2xl shadow-lg"
+             initial={{ opacity: 0, x:100 }}
+             whileInView={{ opacity: 1, x: 0 }}
+             transition={{ duration: 0.7 }}
+             viewport={{ once: false, amount: 0.3 }}
+            >
               <h1 className="text-xl font-semibold mb-8">Redes Sociais</h1>
               <div className="flex gap-4 justify-center">
                 <a className="rounded-full bg-[#333333] p-4 hover:bg-gradient-to-br hover:from-orange-500 hover:to-yellow-400 hover:text-gray-900 hover:shadow-lg shadow-[#ff901288] hover:scale-102 transition-all duration-300 cursor-pointer transform hover:-translate-y-1" href="https://github.com/arnaldoliro" target="_blank" rel="noopener noreferrer"><Github size={20}/></a>
@@ -87,7 +108,7 @@ export default function ContactSection() {
                 <a className="rounded-full bg-[#333333] p-4 hover:bg-gradient-to-br hover:from-orange-500 hover:to-yellow-400 hover:text-gray-900 hover:shadow-lg shadow-[#ff901288] hover:scale-102 transition-all duration-300 cursor-pointer transform hover:-translate-y-1" href="" target="_blank" rel="noopener noreferrer"><Mail size={20}/></a>
                 <a className="rounded-full bg-[#333333] p-4 hover:bg-gradient-to-br hover:from-orange-500 hover:to-yellow-400 hover:text-gray-900 hover:shadow-lg shadow-[#ff901288] hover:scale-102 transition-all duration-300 cursor-pointer transform hover:-translate-y-1" href="" target="_blank" rel="noopener noreferrer"><Send size={20}/></a>
               </div>
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
